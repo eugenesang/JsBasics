e1aee48169f71ab1ae6150cd8ff0b3335bab25c1
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-12-03 22:27:26 +0300

    Clean code

--- attr_smart_check.js
+++ attr_smart_check.js
@@ -1,35 +1,38 @@
-// if we want to perfom some operation with an object's attribute:
-// it is safe to check if the attribte exists to avoid grinding to halts
+// if we want to perform some operation with an object's attribute:
+// it is safe to check if the attribute exists to avoid grinding to halts
 
-let obj={
-  name:{
-    first:"Kiprop",
-    midle:"Eugene",
-    sur:"Sang"
+let obj = {
+  name: {
+    first: "Kiprop",
+    middle: "Eugene",
+    sur: "Sang"
   },
-  gender:"male",
-  age:20
+  gender: "male",
+  age: 20
 };
-function printPersonalInfo(person){
- let info=``;
-  if(person){
-    if(person.name){
-      info+=`name : ${person.name.first} ${person.name.midle} ${person.name.sur}`;
+function printPersonalInfo(person) {
+  let info = ``;
+
+  if (person) {
+
+    if (person.name) {
+      info += `name : ${person.name.first} ${person.name.middle} ${person.name.sur}`;
     }
-    if(person.gender){
-      info+=`
+    if (person.gender) {
+      info += `
       gender: ${person.gender}
       `;
     }
-    if(person.age){
-      info+=` age : ${person.age}`;
+    if (person.age) {
+      info += ` age : ${person.age}`;
+    }
   }
-   console.log(info);
+  console.log(info);
 }
-  
+
 // a more neater way to do it is
-function printInfo(person){
-  console.log("name : ", person.?name.?first, person.?name.?middle, person.?name.?sur);
-  console.log("gender : ", person.?gender);
-  console.log("age : ", person.?age);
+function printInfo(person) {
+  console.log("name : ", person?.name?.first, person?.name?.middle, person?.name?.sur);
+  console.log("gender : ", person?.gender);
+  console.log("age : ", person?.age);
 }
